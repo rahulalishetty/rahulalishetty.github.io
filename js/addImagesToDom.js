b2fa739f98c8634ba1104d4df648e80090e1e40d
rahulalishetty <rahulalishetty1905@gmail.com>
2019-01-21 12:40:54 +0530

    load images from json

--- js/addImagesToDom.js
+++ js/addImagesToDom.js
@@ -11,6 +11,7 @@
 //$(document).ready(function() {
 //    init();
     $.getJSON('../js/images.json', function(data) {
+          var images;
           $.each(data.images, function(i,f) {
                  images+='<div class="main-div-element"><img src="'+ f.path +'"/> <div> '+ f.text+ '</div></div>';
           });
